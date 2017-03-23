9b0236043db8c040c850802cb3f4fd13b70a0971
codiv <codiv@sina.com>
2017-03-23 17:30:23 +0800

    Default

--- vue/codiv/src/components/Hello.vue
+++ vue/codiv/src/components/Hello.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
 
   </div>
 </template>
@@ -17,7 +16,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
